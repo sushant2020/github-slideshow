aacd6fc35430951ec0882de737ac722bcca27558
Anshuman Kumar <anshuman@webdezign.co.uk>
2024-03-20 13:20:01 +0530

    added exports api for trends

--- exports/urls.py
+++ exports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SnapshotRegionExport
+from .views import SnapshotRegionExport, TrendsExport
 
 urlpatterns = [
-    path('snapshotregion',SnapshotRegionExport.as_view(),name='region-export')
+    path('snapshotregion',SnapshotRegionExport.as_view(),name='region-export'),
+    path('trends',TrendsExport.as_view(),name='trends-export')
 ]
