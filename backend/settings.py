c45edb1e4572ac9425cc5b421ac21bd70ccae714
Anshuman Kumar <anshuman@webdezign.co.uk>
2024-03-20 12:25:05 +0530

    added cors allowed header

--- backend/settings.py
+++ backend/settings.py
@@ -77,10 +77,27 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000', 
     "https://api-meaningfulvision.azurewebsites.net" # Add your frontend origin here
 ]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+'Cache-Control'
+]
+
 
 ROOT_URLCONF = 'backend.urls'
 
