242049941f33200de46764ac29976776cbbcf179
Anshuman Kumar <anshuman@webdezign.co.uk>
2024-03-20 17:56:26 +0530

    added user-organization api

--- filters/urls.py
+++ filters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Timescalefitler,CommonFilter,Brand_SegmentFilterAPI,OrganizationDropdown
+from .views import Timescalefitler,CommonFilter,Brand_SegmentFilterAPI,OrganizationDropdown,UserOrganizationDropdown
 
 urlpatterns = [
     path('timescale-filter' , Timescalefitler.as_view(), name='Timescalefilter-api'),
     path('common-filters',CommonFilter.as_view(),name = 'Commonfilters-api'),
     path('brand-filter',Brand_SegmentFilterAPI.as_view(),name = 'brandfitler-api'),
-    path('organization',OrganizationDropdown.as_view(), name = 'organization')
+    path('organization',OrganizationDropdown.as_view(), name = 'organization'),
+    path("user-organization",UserOrganizationDropdown.as_view(),name='user-organization')
 ]   
