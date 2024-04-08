963d2e932e912360ca0ebc1dd6d02c997bb26236
Anshuman Kumar <anshuman@webdezign.co.uk>
2024-04-08 18:13:54 +0530

    added dynamic item filter api

--- filters/urls.py
+++ filters/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Timescalefitler,CommonFilter,Brand_SegmentFilterAPI,OrganizationDropdown,UserOrganizationDropdown
+from .views import Timescalefitler,CommonFilter,Brand_SegmentFilterAPI,OrganizationDropdown,UserOrganizationDropdown,ItemFilterAPI
 
 urlpatterns = [
     path('timescale-filter' , Timescalefitler.as_view(), name='Timescalefilter-api'),
     path('common-filters',CommonFilter.as_view(),name = 'Commonfilters-api'),
     path('brand-filter',Brand_SegmentFilterAPI.as_view(),name = 'brandfitler-api'),
     path('organization',OrganizationDropdown.as_view(), name = 'organization'),
-    path("user-organization",UserOrganizationDropdown.as_view(),name='user-organization')
+    path("user-organization",UserOrganizationDropdown.as_view(),name='user-organization'),
+    path("items",ItemFilterAPI.as_view(), name = "items-api")
 ]   
