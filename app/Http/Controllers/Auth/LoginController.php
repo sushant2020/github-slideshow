e37209dac696371dfa91c5df1784772b2474d87c
Sushant Chari <sushant@webdezign.co.uk>
2024-05-20 18:08:36 +0530

    [TASK] Updated views

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,46 +23,7 @@ class LoginController extends BaseController
     public function login(Request $request)
     {
         //Server side validations
-        $validator = $this->validator($request->all());
-        if ($validator->fails()) {
-            $errors = !empty($validator->messages()->all()) ? $validator->messages()->all() : [];
-            $errorString = $this->errorString($errors);
-            return $this->sendErrorResponse('Validation Error', $errorString, 204);
-        }
-
-        $user = User::where('email', $request->email)->first();
-        if (!$user) {  // if email does not find in database
-            return $this->sendErrorResponse('User Not Found.', 'The user account does not exist in the system', 203);
-        }
-
-        if (!empty($user)) {
-            if ($user->is_deleted == 1) {
-                return $this->sendErrorResponse('User Archived.', 'The user account is disabled or archived', 202);
-            }
-
-            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-                $user = Auth::user();
-
-                if ($user->password_changed == null) {  //check user login first time and set flag 1 after first login
-                    $user->password_changed = 1;
-                    $user->save();
-
-                    $success['first_attempt'] = "true";
-                }
-                $roles = !empty($user) ? $user->getRoleNames()->toArray() : [];
-                $token = trim($user->createToken('ProductPortal')->accessToken, '"');
-
-                $success['token'] = $token;
-
-                $success['name'] = !empty($user) ? $user->getName() : "";
-                $success['email'] = !empty($user) ? $user->email : "";
-                $success['created_at'] = !empty($user) ? $user->created_at : "";
-                $success['roles'] = $roles;
-                return $this->sendResponse($success, 'User login successfully.');
-            } else {
-                return $this->sendErrorResponse('Unauthorised.', 'Please enter your correct password and try again.', 201);
-            }
-        }
+        return $this->sendResponse('test', 'User login successfully.');
     }
 
     /**
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My React App</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
 </head>
 <body>
     <div id="root"></div>
-    <script src="{{ asset('js/main.js') }}"></script> <!-- Adjust the script src if necessary -->
+   <!-- <script src="{{ asset('js/main.js') }}"></script> <!-- Adjust the script src if necessary -->
 </body>
 </html>
