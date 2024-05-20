33ad25066e66ae6ef44d63f261beb897389486ea
Sushant Chari <sushant@webdezign.co.uk>
2024-05-20 18:59:15 +0530

    [TASK] React Laravel set up

--- laravel-backend/routes/web.php
+++ laravel-backend/routes/web.php
@@ -12,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
-
 
 Route::get('/', function () {
     return view('welcome');
